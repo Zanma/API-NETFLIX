a1b993e7292a53284b9c2fbab3adb7e1b674d7c2
Zainuri Mahmud <zainuri.mahmud@bni.co.id>
2024-07-29 13:19:01 +0700

    create api endpoint

--- index.js
+++ index.js
@@ -3,7 +3,35 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({ message: "Hello World!" });
+});
+
+//melihat
+app.get("/my-movies", (req, res) => {
+  res.json({ message: "List of movies" });
+});
+
+//membuat
+app.post("/my-movies", (req, res) => {
+  const data = req.body;
+  console.log({ data });
+  res.status(201).json({ message: "success" });
+});
+
+//menghapus
+app.delete("/my-movies/:id/:token", (req, res) => {
+  console.log("delete movies");
+  const { id, token } = req.params;
+  console.log({ id, token });
+  res.status(204).json({ message: "success" });
+});
+
+//menambahkan data token
+app.post("/token", (req, res) => {
+  console.log("add token");
+  const data = req.body;
+  console.log({ data });
+  res.status(201).json({ message: "token created..." });
 });
 
 app.listen(port, () => {
